c5256475352f97ccdbfc347d2caef749bb74906b
VladimirDanil <vladimir.danil25@mail.ru>
2021-06-10 09:58:08 -0700

    some fix

--- src/containers/auth/widgets/AddressProfile.js
+++ src/containers/auth/widgets/AddressProfile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Field, reduxForm } from 'redux-form';
-
+    
 import RenderField from '../../../components/renderField';
 
 const checkValidate = (value, property = []) => {
